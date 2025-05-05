58a7b86791e6ce24d0ea4f8b08853000c015292c
Sachin <sachinfnatic@gmail.com>
2025-05-06 01:55:37 +0530

    update: grenade support

--- Source Code/NoBlock.cs
+++ Source Code/NoBlock.cs
@@ -17,19 +17,39 @@ public class NoBlock : BasePlugin
     public override string ModuleDescription => "Allows for players to walk through each other without being stopped due to colliding.";
     public override string ModuleVersion => "V1.0.2";
 
-    private readonly int OnCollisionRulesChangedOffset = GameData.GetOffset("OnCollisionRulesChangedOffset");
-
     public FakeConVar<bool> cEnable = new("css_noblock_enable", "Toggle between noblock", true);
+    public FakeConVar<bool> cGrenadeEnable = new("css_noblock_grenade_enable", "Toggle between noblock for grenades", true);
+
+    public override void Load(bool hotReload)
+    {
+        base.Load(hotReload);
+        RegisterListener<Listeners.OnEntityCreated>(OnEntityCreated);
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        base.Unload(hotReload);
+        RemoveListener<Listeners.OnEntityCreated>(OnEntityCreated);
+    }
+
+    private void OnEntityCreated(CEntityInstance entity)
+    {
+        if (!cGrenadeEnable.Value) return;
+
+        var className = entity.Entity?.DesignerName ?? "";
+
+        if (string.IsNullOrWhiteSpace(className)) return;
+
+        if (className.Contains("_projectile"))
+            entity.As<CBaseEntity>().SetCollisionGroup(CollisionGroup.COLLISION_GROUP_NEVER);
+    }
 
     [GameEventHandler]
     public HookResult Event_PlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
     {
         var player = @event.Userid;
 
-        if (player == null || !player.IsValid)
-        {
-            return HookResult.Continue;
-        }
+        if (player == null || !player.IsValid) return HookResult.Continue;
 
         CHandle<CCSPlayerPawn> pawn = player.PlayerPawn;
 
@@ -40,25 +60,32 @@ public class NoBlock : BasePlugin
 
     private void PlayerSpawnNextFrame(CCSPlayerController player, CHandle<CCSPlayerPawn> pawn)
     {
-        if(player == null || !player.IsValid || pawn == null || !pawn.IsValid || pawn.Value == null || !pawn.Value.IsValid || pawn.Value.Collision.Handle == IntPtr.Zero) return;
+        if(player == null
+        || !player.IsValid
+        || pawn == null
+        || !pawn.IsValid
+        || pawn.Value == null
+        || !pawn.Value.IsValid) return;
 
         CollisionGroup collision = CollisionGroup.COLLISION_GROUP_PLAYER;
 
-        if(cEnable.Value)
-        {
-            collision = CollisionGroup.COLLISION_GROUP_DEBRIS;
-        }
+        if(cEnable.Value) collision = CollisionGroup.COLLISION_GROUP_DEBRIS;
 
-        if(pawn.Value.Collision.CollisionGroup != (byte)collision)
-        {
-            pawn.Value.Collision.CollisionGroup = (byte)collision;
-            pawn.Value.Collision.CollisionAttribute.CollisionGroup = (byte)collision;
+        if(pawn.Value.Collision.CollisionGroup != (byte)collision) pawn.Value.SetCollisionGroup(collision);
+    }
+}
 
-            Utilities.SetStateChanged(pawn.Value, "CCollisionProperty", "m_CollisionGroup");
-            Utilities.SetStateChanged(pawn.Value, "CCollisionProperty", "m_collisionAttribute");
+public static class Extensions
+{
+    private static readonly int OnCollisionRulesChangedOffset = GameData.GetOffset("OnCollisionRulesChangedOffset");
+    public static void SetCollisionGroup(this CBaseEntity entity, CollisionGroup collision)
+    {
+        if(entity == null || !entity.IsValid || entity.Handle == IntPtr.Zero || entity.Collision == null || entity.Collision.Handle == IntPtr.Zero) return;
 
-            VirtualFunctionVoid<nint> collisionRulesChanged = new VirtualFunctionVoid<nint>(pawn.Value.Handle, OnCollisionRulesChangedOffset);
-            collisionRulesChanged.Invoke(pawn.Value.Handle);
-        }
+        entity.Collision.CollisionGroup = (byte)collision;
+        Utilities.SetStateChanged(entity, "CCollisionProperty", "m_collisionAttribute");
+
+        VirtualFunctionVoid<nint> collisionRulesChanged = new VirtualFunctionVoid<nint>(entity.Handle, OnCollisionRulesChangedOffset);
+        collisionRulesChanged.Invoke(entity.Handle);
     }
 }
